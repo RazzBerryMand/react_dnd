f8293ea0c4dfdb305c4a108f86d703cd59952bfd
Mand <amanda.cashin@codecomputerlove.com>
2019-06-10 17:46:27 +0100

    set up app.js and add target.js and items.js

---
+++ src/Item.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Preview extends Component {
+  render() {
+    return <div className="item">{this.props.item.name}</div>;
+  }
+}
+
+export default Preview;
