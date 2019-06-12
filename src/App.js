676e660aa435b502f8730e52595f007270132869
Mand <amanda.cashin@codecomputerlove.com>
2019-06-12 10:44:01 +0100

    add card.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Item from "./Item.js";
 import Target from "./Target.js";
+import Card from "./Card";
 import { DragDropContext } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 
@@ -12,6 +13,36 @@ class App extends Component {
       { id: 2, name: "Item 2" },
       { id: 3, name: "Item 3" },
       { id: 4, name: "Item 4" }
+    ],
+    cards: [
+      {
+        id: 1,
+        text: "Go to work"
+      },
+      {
+        id: 2,
+        text: "Drink bubble tea"
+      },
+      {
+        id: 3,
+        text: "Watch the goldbergs"
+      },
+      {
+        id: 4,
+        text: "Play pokemon"
+      },
+      {
+        id: 5,
+        text: "Go to nandos"
+      },
+      {
+        id: 6,
+        text: "Go home"
+      },
+      {
+        id: 7,
+        text: "Go to sleep"
+      }
     ]
   };
 
@@ -30,12 +61,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Drag and drop example</p>
+          <h1>Drag and drop examples:</h1>
+          <h2>Recycle bin and re-ordering a list</h2>
         </header>
 
         <div className="app-intro">
           <div className="app-container">
-            <div className="item-container">
+            {/* <div className="item-container">
               {this.state.items.map((item, index) => (
                 <Item
                   key={item.id}
@@ -43,8 +75,20 @@ class App extends Component {
                   handleDrop={id => this.deleteItem(id)}
                 />
               ))}
-            </div>
-            <Target />
+            </div> */}
+            {/* <Target /> */}
+          </div>
+
+          <div className="card-container">
+            {this.state.cards.map((card, i) => (
+              <Card
+                key={card.id}
+                index={i}
+                id={card.id}
+                text={card.text}
+                moveCard={this.moveCard}
+              />
+            ))}
           </div>
         </div>
       </div>
